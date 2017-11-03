8e0c27fa73bfb3d344bbc0e789c5d33bd6f35f79
ammygill <31243066+ammygill@users.noreply.github.com>
2017-11-03 12:40:08 +0530

    Update lvm-filter.sh

--- lvm-filter.sh
+++ lvm-filter.sh
@@ -1,21 +1,23 @@
-
+#defining Colour
 red=`tput setaf 1`
 green=`tput setaf 2`
 normal=`tput sgr0`
 
+#Checking the Root file system and redirecting that to a file.
 pvs -a --config 'devices { filter = [ "a|.*|" ] }' --noheadings \
     -opv_name,fmt,vg_name | awk 'BEGIN { f = ""; } \
     NF == 3 { n = "\42a|"$1"|\42, "; f = f n; } END \
         { print " "f"\"r|.*|\" ]" }' > /tmp/lvm-device-filter
 
+#Checking the actual file for uncommented lines of filter in /etc/lvm/lvm.conf
 grep -i "filter" /etc/lvm/lvm.conf | grep -v "#" > /tmp/uncommented_filter.txt
 
-
+#If no filter statement in /etc/lvm/lvm.conf is uncommented
 if [ $(echo $?) != 0  ]
     then
-    echo "Your System will come Up there is no issue in the System Regarding Lvm Filter"
+    echo "${red}Your System will come Up there is no issue in the System Regarding Lvm Filter${normal}"
 else
-
+    #
     for i in $(cat /tmp/lvm-device-filter)
     do
             if [ $i != `awk '{print $NF}' /tmp/lvm-device-filter` ]
